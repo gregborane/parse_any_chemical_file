6ed48bdb9087bc1e008f0cd2bfc1d09410c57ea6
gregborane <madaradellac@gmail.com>
2025-12-05 17:33:06 +0100

    small mistakes

--- molecule_builder.py
+++ molecule_builder.py
@@ -16,7 +16,6 @@ with open("./constant/covalences.json", "r") as p_table:
 list_p_table_keys = list(periodic_table.keys())
 
 
-
 # %%
 def get_molecule(content: str | list) -> tuple:
     from parse_any_file import parse_any_file
@@ -46,7 +45,9 @@ def compute_distance(
 
 
 # %%
-def generate_bonds(content: str|list, tolerance: np.float16 = np.float16(0.1)):
+def generate_bonds(
+    content: str | list, tolerance: np.float16 = np.float16(0.1)
+) -> tuple:
     global periodic_table, list_p_table_keys
 
     coord, atoms, bonds, double_bonds, triple_bonds = get_molecule(content)
@@ -85,7 +86,7 @@ def generate_bonds(content: str|list, tolerance: np.float16 = np.float16(0.1)):
 
     Such determination depends on the valencies considered for the atoms. Hypervalencies atoms will not be taken into account. 
     """
-    
+
     bond_count = defaultdict(int)
     for i, j in bonds:
         bond_count[i] += 1
@@ -94,25 +95,20 @@ def generate_bonds(content: str|list, tolerance: np.float16 = np.float16(0.1)):
     for i in range(len(atoms)):
         elem1 = list_p_table_keys[atoms[i]]
         if elem1 == "C" and bond_count[i] == 3:
-            for j in list(b for a, b in bonds if a == i) + list(a for a, b in bonds if b == i):
+            for j in list(b for a, b in bonds if a == i) + list(
+                a for a, b in bonds if b == i
+            ):
                 valencies = periodic_table[list_p_table_keys[j]]["covalence"]
-                if (bond_count[j] < valencies and (i, j) not in double_bonds):
+                if bond_count[j] < valencies and (i, j) not in double_bonds:
                     double_bonds.add((i, j))  # Convert single bond to double
                     bond_count[i] += 1
                     bond_count[j] += 1
                     break
-    
+
     return bonds, double_bonds, triple_bonds
 
 
-def get_graph(content : str|list) -> nx.Graph:
-    """
-    Convert xyz file to a nx.graph object
-    bonds are edges, nodes are atoms
-    cat (dict) : dict containing a four coordinates object with atoms and its respective coordinates
-    return G (nx.Graph) : graph object of the molecule
-    """
-
+def get_graph(content: str | list) -> nx.Graph:
     coord, atoms, bonds, double_bonds, triple_bonds = get_molecule(content)
     bonds, double_bonds, triple_bonds = generate_bonds(content)
     G = nx.Graph()
@@ -123,41 +119,30 @@ def get_graph(content : str|list) -> nx.Graph:
 
     # Add edges for single bonds
     for bond in bonds:
-        if bond in double_bonds:
+        if bond in triple_bonds:
             u, v = bond
-            bond_type = (
-                "double"  
-            )
+            bond_type = "triple"
             G.add_edge(u, v, bond_type=bond_type)
-        elif bond in triple_bonds:
+        elif bond in double_bonds:
             u, v = bond
-            bond_type = (
-                "triple" 
-            )
+            bond_type = "double"
             G.add_edge(u, v, bond_type=bond_type)
         else:
             u, v = bond
-            bond_type = "single"  
+            bond_type = "single"
             G.add_edge(u, v, bond_type=bond_type)
     return G
 
 
-def nx_to_mol(G: nx.Graph):
-    """
-    Convert a networkx graph boject to mol one
-    G (nx.graph): graph to convert
-    return mol : rdkit mol object
-    """
-    
+def nx_to_mol(G: nx.Graph) -> Chem.RWMol:
     bond_type_map = {
         "single": Chem.BondType.SINGLE,
         "double": Chem.BondType.DOUBLE,
-        "triple": Chem.BondType.TRIPLE
+        "triple": Chem.BondType.TRIPLE,
     }
 
     mol = Chem.RWMol()
     atomic_nums = nx.get_node_attributes(G, "element")
-    # atomic_nums = [periodic_table[atom] for atom in atoms.values()]
     node_to_idx = {}
     for node in G.nodes():
         a = Chem.Atom(atomic_nums[node])
@@ -175,19 +160,20 @@ def nx_to_mol(G: nx.Graph):
     Chem.SanitizeMol(mol)
     return mol
 
-def main(content : str|list) : 
+
+def main(content: str | list) -> dict:
     coord, atoms, bonds, double_bonds, triple_bonds = get_molecule(content)
     bonds, double_bonds, triple_bonds = generate_bonds(content)
-    
+
     graph_mol = get_graph(content)
     rdkit_mol = nx_to_mol(graph_mol)
 
     return {
-            "coord"       : coord,
-            "atoms"       : atoms,
-            "bonds"       : bonds,
-            "double_bonds": double_bonds,
-            "triple_bonds": triple_bonds,
-            "graph_mol"   : graph_mol,
-            "rdkit_mol"   : rdkit_mol,
+        "coord": coord,
+        "atoms": atoms,
+        "bonds": bonds,
+        "double_bonds": double_bonds,
+        "triple_bonds": triple_bonds,
+        "graph_mol": graph_mol,
+        "rdkit_mol": rdkit_mol,
     }
